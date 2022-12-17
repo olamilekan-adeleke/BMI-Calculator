f38c386933ba01538f90e0970d62e6f508cec7c5
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-12-17 09:30:16 +0100

    update

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var heightSliderOutlet: UISlider!
+    @IBOutlet var weightSliderOutlet: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightSliderOutlet.value = 0
+        weightSliderOutlet.value = 0
     }
 
-
+    @IBAction func onSliderChanged(_ sender: UISlider) {}
 }
-
